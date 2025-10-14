75c878c0f91dc3f0fcdc7817ff1d5cff80b073ef
Vinícius Cardoso da Rosa <vinidarosa.for@gmail.com>
2025-10-14 14:41:46 -0300

    menu e pontos

--- Projeto-Vienna/Assets/sprites/Script/GameManager.cs
+++ Projeto-Vienna/Assets/sprites/Script/GameManager.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public int pontos = 0;
     public int vidas = 3;
-    
+    public TextMeshProUGUI textPontos;
+    public TextMeshProUGUI textVida;
+
     public void addpontos(int qtd/*quantidade*/) 
     {
         pontos += qtd;
         if (pontos < 0) { pontos = 0; }
+        textPontos.text = "Pontos: " + pontos;
         Debug.Log("pontos: " + pontos);
     }
     public void alteravida(int vida) 
     { 
         vidas -= vida;
+        textVida.text = "Vidas: " + vidas;
         Debug.Log("Vidas: " + vidas);
         GameObject player = GameObject.FindWithTag ("Player" );
         player.GetComponent<Player>().ReiniciarPosição();
---
+++ Projeto-Vienna/Assets/sprites/Script/Menu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    
+    void Start()
+    {
+   
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
